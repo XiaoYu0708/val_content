e84dd8591bc6f1d67a156a6697269fafb469300e
weitsungyu <weitsungyu@gmail.com>
2024-02-04 14:05:41 +0000

    Update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,16 +8,16 @@
   idx.previews = {
     enable = true;
     previews = [
-        {
-            command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
-            id = "web";
-            manager = "flutter";
-        }
-        {
-            command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
-            id = "android";
-            manager = "flutter";
-        }
+        # {
+        #     command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
+        #     id = "web";
+        #     manager = "flutter";
+        # }
+        # {
+        #     command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+        #     id = "android";
+        #     manager = "flutter";
+        # }
         {
             id = "ios";
             manager = "ios";
